350c3f4755d954a71d2245085f9f6e618873e3ee
James <jamesliaw10@gmail.com>
2023-08-29 22:53:59 -0700

    Menu API and Table booking API set up

--- Capstone_project/restaurant/views.py
+++ Capstone_project/restaurant/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
+from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Menu, Booking
+from .serializer import MenuItemSerializer, BookingSerializer
+
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuItemSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuItemSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
